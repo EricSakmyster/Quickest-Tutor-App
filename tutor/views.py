831361216269a1d8925535c7a486056e5dc64a55
Abhi Dommalapati <ad4bu@virginia.edu>
2020-02-17 02:40:50 -0500

    add signup page

--- tutor/views.py
+++ tutor/views.py
@@ -7,8 +7,12 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Welcome to the Tutor App!")
 
+def signup(request):
+    return HttpResponse("Welcome to the Signup Page")
+
 def profile(request):
     return HttpResponse("Student Tab Profile")
 
 def schedule(request):
     return HttpResponse("Student Tab Schedule")
+
