aa50c77d02244b6f4ccd64689d18d4fe5cb111ff
Eric Sakmyster <ems5fa@virginia.edu>
2020-02-16 19:44:17 -0500

    Added tutor search page

--- tutor/urls.py
+++ tutor/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('student/profile', views.profile, name='profile'),
     path('student/schedule', views.schedule, name='schedule'),
+    path('student/tutorsearch', views.tutorsearch, name='tutorsearch'),
 ]
