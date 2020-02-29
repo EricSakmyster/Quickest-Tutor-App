935564d9d33f4b17816ccfd80a0913ee6aae09bb
Eric Sakmyster <ems5fa@virginia.edu>
2020-02-28 23:52:28 -0500

    Added another dummy test

--- home/tests.py
+++ home/tests.py
@@ -4,3 +4,7 @@ from django.test import TestCase
 class DummyTest(TestCase):
     def test1(self):
         self.assertEqual(1,1)
+
+class DummyTest2(TestCase):
+    def test2(self):
+        self.assertEqual(2,2)
