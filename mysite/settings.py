cea603b0cee1f15a135e5a4aa9e83e3700870fba
Abhi Dommalapati <ad4bu@virginia.edu>
2020-02-29 19:05:41 -0500

    fixed runserver error

--- mysite/settings.py
+++ mysite/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'tutordatabase',                      
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'quickesttutor',
+        'PASSWORD': 'qt3',
         'HOST': 'localhost',
         'PORT': '5432',
     }
