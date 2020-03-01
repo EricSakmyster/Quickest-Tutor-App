9d0949098d9dad4c59a67a65fe3d4f1dcff6eec4
Eric Sakmyster <ems5fa@virginia.edu>
2020-02-29 23:47:24 -0500

    fixing travis

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'tutor.apps.TutorConfig',
+    'home.apps.TutorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
