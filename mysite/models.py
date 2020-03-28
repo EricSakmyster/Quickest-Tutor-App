4081ba058fbdfda096879596ca3c56651fd8f2ce
Eric Sakmyster <ems5fa@virginia.edu>
2020-03-28 14:30:23 -0400

    Added to-do list, isn't fully functional

--- mysite/models.py
+++ mysite/models.py
@@ -2,13 +2,3 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
-
-
-# class Student(models.Model):
-#     student_name = models.CharField(max_length=200)
-#     student_year =  models.CharField(max_length=200)
-
-#     def __str__(self):
-#         return self.student_name
-#     def _year_(self):
-#         return self.student_year
