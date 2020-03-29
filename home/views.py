45370c34aa2e870905a198fd870b8b638007be8d
Abhi Dommalapati <ad4bu@virginia.edu>
2020-03-29 01:29:47 -0400

    fixed tutor session functionality

--- home/views.py
+++ home/views.py
@@ -49,10 +49,10 @@ def index(request): #the index view
             content = title + " -- " + date + " " + category #content
             Todo = TodoList(title=title, content=content, due_date=date, category=Category.objects.get(name=category))
             Todo.save() #saving the todo 
-            return redirect("/") #reloading the page
+            return redirect("home/studentSchedule") #reloading the page
         if "taskDelete" in request.POST: #checking if there is a request to delete a todo
             checkedlist = request.POST["checkedbox"] #checked todos to be deleted
             for todo_id in checkedlist:
                 todo = TodoList.objects.get(id=int(todo_id)) #getting todo id
                 todo.delete() #deleting todo
-    return render(request, "index.html", {"todos": todos, "categories":categories})
+    return render(request, "home/studentSchedule.html", {"todos": todos, "categories":categories})
