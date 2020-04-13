7c2385467b3cef4029a9bf841dab2e5146fc7627
hjb4ab <56943922+hjb4ab@users.noreply.github.com>
2020-04-13 15:22:56 -0400

    adding image back

--- home/forms.py
+++ home/forms.py
@@ -6,7 +6,7 @@ from django.forms.widgets import FileInput
 class TutorProfileForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['phone', 'major', 'tsubjects', 'texp', 'hourlyRate']
+        fields = ['phone', 'major', 'tsubjects', 'texp', 'hourlyRate', 'image']
         widgets = {
             'phone': forms.NumberInput(attrs={'class': "form-control", 'placeholder': "XXX-XXX-XXXX"}),
             'major': forms.TextInput(attrs={'class': "form-control", 'placeholder': "ex) Math, Chemistry"}),
